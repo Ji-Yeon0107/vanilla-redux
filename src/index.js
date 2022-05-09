2502a2c258e7b929fe685c54bbf934e7edede20b
BakJiYeon <jiyeon@192.168.nate.com>
2022-05-09 09:44:14 +0900

    commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (state = 0) => {
+  return state;
+};
+const countStore = createStore(countModifier);
